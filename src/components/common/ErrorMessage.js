afa3c86e393117a6fe34043f3e43967a6f6b6f9d
koteswararao <koteediga85@gmail.com>
2025-09-15 19:37:37 +0530

    Done with Final Functional Requirement

--- src/components/common/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorMessage = ({ message }) => (
-  <div style={{ color: 'red', textAlign: 'center', margin: 16 }}>
-    {message}
-  </div>
-);
-
-export default ErrorMessage;
--- src/components/common/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Loader = () => (
-  <div style={{ textAlign: 'center', margin: 24, fontSize: 24 }}>
-    Loading...
-  </div>
-);
-
-export default Loader;
